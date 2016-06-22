0233d589878449465396bb91d9c3d6abc354a015
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-06-22 11:08:15 +0800

    add main application

---
+++ product-service/src/main/java/com/wow/product/ProductApplication.java
@@ -0,0 +1,65 @@
+package com.wow.product;
+
+import com.wow.product.model.Product;
+import com.wow.product.service.ProductService;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableTransactionManagement
+@MapperScan("com.wow.product.mapper")
+public class ProductApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProductApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "/test-product/{productId}", method = RequestMethod.GET)
+    public Product getPrimarySubProduct(@PathVariable Integer productId) {
+        System.out.println("accept request:" + productId);
+        return productService.queryPrimarySubProductOfProductSerial(productId);
+    }
+
+//    @Bean(name = "productDataSource")
+//    @Primary
+//    @ConfigurationProperties(prefix = "spring.datasource.product")
+//    public DataSource productDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+//
+//    @Bean(name = "productSqlSessionFactory")
+//    @Primary
+//    public SqlSessionFactory productSqlSessionFactory(@Qualifier("productDataSource") DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+//        bean.setDataSource(dataSource);
+//        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:/mapper/product/*Mapper.xml"));
+//        return bean.getObject();
+//    }
+//
+//    @Bean(name = "productTransactionManager")
+//    @Primary
+//    public DataSourceTransactionManager productTransactionManager(@Qualifier("productDataSource") DataSource dataSource) {
+//        return new DataSourceTransactionManager(dataSource);
+//    }
+//
+//    @Bean(name = "productSqlSessionTemplate")
+//    @Primary
+//    public SqlSessionTemplate productSqlSessionTemplate(@Qualifier("productSqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+//        return new SqlSessionTemplate(sqlSessionFactory);
+//    }
+}
