de9d3d89af4dde62e6c9e2ee1f9eff605c28e06e
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-06-24 20:52:11 +0800

    modify table structure

--- src/main/java/com/wow/mobileapi/interceptor/MustLoginInterceptor.java
+++ src/main/java/com/wow/mobileapi/interceptor/MustLoginInterceptor.java
@@ -1,36 +1,56 @@
 package com.wow.mobileapi.interceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.wow.user.model.EndUser;
+import com.wow.user.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.ActuatorMetricWriter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * 拦截未登录的用户信息
- *
- * @author lance
- *         2014-6-10下午9:57:20
+ * 拦截未登录的用户
  */
 public class MustLoginInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(MustLoginInterceptor.class);
+
+    @Autowired
+    private LoginService loginService;
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws Exception {
 
         logger.info("must login interceptor:preHandle:" + request);
-        //验证用户是否登陆
-        Object obj = request.getSession().getAttribute("cur_user");
-        if (obj == null || !(obj instanceof EndUser)) {
+        //验证用户是否登陆 - 从cookie或者请求体中获取token,通过redis或mysql检查该token是否有效
+        String token = "";
+        boolean isValidToken = false;
+//        Object obj = request.getSession().getAttribute("cur_user");
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("session")) {
+                token = cookie.getValue();
+                break;
+            }
+        }
+        if ("".equals(token)) {
+            //从请求体中获取
+            token = request.getParameter("token");
+        }
+        //check whether token is valid, by search it from redis or mysql
+        if (! isValidToken) {
             logger.info("you must login to continue...");
-//            response.sendRedirect(request.getContextPath() + "/login");
             response.getWriter().write("You need to login to continue");
             return false;
+        } else {
+            //判断用户过期时间,如果快要过期了,延长过期时间
+            loginService.refreshSession(token);
         }
 
         return true;
