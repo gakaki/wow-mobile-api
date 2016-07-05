7a2cbe1e0977454925934781fd6502f5cef08904
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-05 18:28:22 +0800

    add stock controller

---
+++ src/main/java/com/wow/mobileapi/controller/StockController.java
@@ -0,0 +1,36 @@
+package com.wow.mobileapi.controller;
+
+import com.wow.mobileapi.dto.ApiResponse;
+import com.wow.mobileapi.util.ResponseUtil;
+import com.wow.stock.service.StockService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by zhengzhiqing on 16/6/23.
+ */
+@RestController
+@RequestMapping("/v1/stocks")
+public class StockController {
+    private static final Logger logger = LoggerFactory.getLogger(StockController.class);
+
+    @Autowired
+    private StockService stockService;
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    public ApiResponse getProductWarehouses(@PathVariable Integer productId) {
+        logger.info("start to get warehouses in which product stored");
+        ApiResponse apiResponse = new ApiResponse();
+        List<Integer> warehouseList = stockService.selectWarehouseByProductId(productId);
+        ResponseUtil.setResponse(apiResponse,"0");
+        apiResponse.setData(warehouseList);
+        return apiResponse;
+    }
+}
