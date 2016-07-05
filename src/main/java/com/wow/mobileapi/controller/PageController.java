f84927ea15e2716f8e2bbe636845f7fe4e690cf5
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-05 18:13:45 +0800

    refactor url

--- src/main/java/com/wow/mobileapi/controller/PageController.java
+++ src/main/java/com/wow/mobileapi/controller/PageController.java
@@ -1,11 +1,15 @@
 package com.wow.mobileapi.controller;
 
+import com.wow.mobileapi.dto.ApiResponse;
+import com.wow.mobileapi.util.ResponseUtil;
 import com.wow.page.model.PageBannerConfig;
 import com.wow.page.model.PageSceneConfig;
 import com.wow.page.model.PageTopicConfig;
+import com.wow.page.service.PageConfigService;
 import com.wow.product.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +18,30 @@ import java.util.List;
  * Created by zhengzhiqing on 16/6/23.
  */
 @RestController
-@RequestMapping
+@RequestMapping("/v1/pages")
 public class PageController {
     private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 
-    @RequestMapping(value = "/v1/pages/banners?pageType={pageType}", method = RequestMethod.GET)
-    public List<PageBannerConfig> getBannersOnPage(@RequestParam Integer pageType) {
+    @Autowired
+    private PageConfigService pageConfigService;
+
+    @RequestMapping(value = "/type/{pageType}/banners", method = RequestMethod.GET)
+    public ApiResponse getBannersOnPage(@PathVariable Integer pageType) {
+        logger.info("start to get banners on page");
+        ApiResponse apiResponse = new ApiResponse();
+        List<PageBannerConfig> bannerList = pageConfigService.getBannersByPageType(pageType);
+        ResponseUtil.setResponse(apiResponse,"0");
+        apiResponse.setData(bannerList);
+        return apiResponse;
+    }
+
+    @RequestMapping(value = "/type/{pageType}/scenes", method = RequestMethod.GET)
+    public List<PageSceneConfig> getScenesOnPage(@PathVariable Integer pageType) {
         return null;
     }
 
-    @RequestMapping(value = "/v1/pages/scenes?pageType={pageType}", method = RequestMethod.GET)
-    public List<PageSceneConfig> getScenesOnPage(@RequestParam Integer pageType) {
-        return null;
-    }
-
-    @RequestMapping(value = "/v1/pages/topics?pageType={pageType}", method = RequestMethod.GET)
-    public List<PageTopicConfig> getTopicsOnPage(@RequestParam Integer pageType) {
+    @RequestMapping(value = "/type/{pageType}/topics", method = RequestMethod.GET)
+    public List<PageTopicConfig> getTopicsOnPage(@PathVariable Integer pageType) {
         return null;
     }
 }
