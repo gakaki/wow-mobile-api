d63e2e7d73b94521762bb033192867722b2cde27
gakaki <gakaki@gmail.com>
2016-07-05 17:20:47 +0800

    change nexus url link address

--- user-service/src/main/java/com/wow/user/thirdparty/SmsSender.java
+++ user-service/src/main/java/com/wow/user/thirdparty/SmsSender.java
@@ -1,9 +1,9 @@
 package com.wow.user.thirdparty;
 
-//import com.taobao.api.DefaultTaobaoClient;
-//import com.taobao.api.TaobaoClient;
-//import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
-//import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
+import com.taobao.api.response.AlibabaAliqinFcSmsNumSendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,23 +27,23 @@ public class SmsSender {
     private static final Logger logger = LoggerFactory.getLogger(SmsSender.class);
 
     /**
-     * 发送验证码
-     * @param req
-     * @return
-     */
-//    public String sendValidateCode(AlibabaAliqinFcSmsNumSendRequest req) {
-//        TaobaoClient client = new DefaultTaobaoClient(serverUrl, appKey, appSecret);
-//        AlibabaAliqinFcSmsNumSendResponse rsp;
-//        String res = "";
-//        try {
-//            rsp = client.execute(req);
-//            res = rsp.getBody();
-//            logger.info(res);
-//            return res;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }finally {
-//            return res;
-//        }
-//    }
+    * 发送验证码
+    * @param req
+    * @return
+    */
+    public String sendValidateCode(AlibabaAliqinFcSmsNumSendRequest req) {
+        TaobaoClient client = new DefaultTaobaoClient(serverUrl, appKey, appSecret);
+        AlibabaAliqinFcSmsNumSendResponse rsp;
+        String res = "";
+        try {
+            rsp = client.execute(req);
+            res = rsp.getBody();
+            logger.info(res);
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            return res;
+        }
+    }
 }
