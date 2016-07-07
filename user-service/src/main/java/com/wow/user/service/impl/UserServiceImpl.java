b103b106f4a756726f77131a3a2a0b9173141f41
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-07 11:38:09 +0800

    captcha generation

--- user-service/src/main/java/com/wow/user/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/wow/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.wow.user.service.impl;
 
 import com.taobao.api.request.AlibabaAliqinFcSmsNumSendRequest;
 import com.wow.common.error.ErrorRepositoryManager;
+import com.wow.common.util.RandomGenerator;
 import com.wow.common.util.RedisUtil;
 import com.wow.user.mapper.EndUserMapper;
 import com.wow.user.model.*;
@@ -137,23 +138,23 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 请求验证码
+     * 调用阿里大鱼的短信接口,往目标手机发送随机生成的6位数字,并将6位数字存储到Redis中
      *
      * @param mobile
      * @return 验证码
      */
     public String getCaptcha(String mobile) {
 
-        //TODO:调用阿里大禹的短信接口,往目标手机发送随机生成的6位数字,并将6位数字存储到Redis中
         //1. generate 6-bit digit randomly
-        String randomNum = "349873";
+        String randomNum = RandomGenerator.createRandom(true,6);
 
         //2. call alidayu interface to send sms
         AlibabaAliqinFcSmsNumSendRequest req = new AlibabaAliqinFcSmsNumSendRequest();
         req.setExtend( "13764641531 uid " );//公共回传参数，在“消息返回”中会透传回该参数；举例：用户可以传入自己下级的会员ID，在消息返回时，该会员ID会包含在内，用户可以根据该会员ID识别是哪位会员使用了你的应用
         req.setSmsType("normal");//短信类型，传入值请填写normal
-        req.setSmsFreeSignName("尖叫设计");//   短信签名，传入的短信签名必须是在阿里大鱼“管理中心-短信签名管理”中的可用签名。如“阿里大鱼”已在短信签名管理中通过审核，则可传入”阿里大鱼“（传参时去掉引号）作为短信签名。短信效果示例：【阿里大鱼】欢迎使用阿里大鱼服务。
-        req.setSmsParamString("{'code':'743621','product':'尖叫设计'}");//短信模板变量，传参规则{"key":"value"}，key的名字须和申请模板中的变量名一致，多个变量之间以逗号隔开。示例：针对模板“验证码${code}，您正在进行${product}身份验证，打死不要告诉别人哦！”，传参时需传入{"code":"1234","product":"alidayu"}
-        req.setRecNum("13764641531");//短信接收号码。支持单个或多个手机号码，传入号码为11位手机号码，不能加0或+86。群发短信需传入多个号码，以英文逗号分隔，一次调用最多传入200个号码。示例：18600000000,13911111111,13322222222
+        req.setSmsFreeSignName("尖叫设计");//短信签名，传入的短信签名必须是在阿里大鱼“管理中心-短信签名管理”中的可用签名。如“阿里大鱼”已在短信签名管理中通过审核，则可传入”阿里大鱼“（传参时去掉引号）作为短信签名。短信效果示例：【阿里大鱼】欢迎使用阿里大鱼服务。
+        req.setSmsParamString("{'code':'" + randomNum + "','product':'尖叫设计'}");//短信模板变量，传参规则{"key":"value"}，key的名字须和申请模板中的变量名一致，多个变量之间以逗号隔开。示例：针对模板“验证码${code}，您正在进行${product}身份验证，打死不要告诉别人哦！”，传参时需传入{"code":"1234","product":"alidayu"}
+        req.setRecNum(mobile);//短信接收号码。支持单个或多个手机号码，传入号码为11位手机号码，不能加0或+86。群发短信需传入多个号码，以英文逗号分隔，一次调用最多传入200个号码。示例：18600000000,13911111111,13322222222
         req.setSmsTemplateCode("SMS_5165048");//短信模板ID，传入的模板必须是在阿里大鱼“管理中心-短信模板管理”中的可用模板。示例：SMS_585014
         smsSender.sendValidateCode(req);
 
