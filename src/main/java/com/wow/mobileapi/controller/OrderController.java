7731d6168b6928f668033ebf0721857131edae10
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-12 12:11:12 +0800

    refine code

--- src/main/java/com/wow/mobileapi/controller/OrderController.java
+++ src/main/java/com/wow/mobileapi/controller/OrderController.java
@@ -1,16 +1,17 @@
 package com.wow.mobileapi.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.wow.common.request.ApiRequest;
 import com.wow.common.response.ApiResponse;
 import com.wow.common.util.JsonUtil;
 import com.wow.order.service.OrderService;
 import com.wow.order.vo.request.OrderRequest;
 import com.wow.order.vo.response.OrderResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zhengzhiqing on 16/7/2.
@@ -19,7 +20,7 @@ import com.wow.order.vo.response.OrderResponse;
 @RequestMapping
 public class OrderController extends BaseController {
 
-    //private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     private OrderService orderService;
@@ -27,31 +28,33 @@ public class OrderController extends BaseController {
     /**
      * 获取订单列表信息
      * 
-     * @param orderId
+     * @param request
      * @return
      */
     @RequestMapping(value = "/v1/orders", method = RequestMethod.GET)
     public ApiResponse getOrderList(ApiRequest request) {
         OrderRequest orderRequest = JsonUtil.fromJSON(request.getParamJson(), OrderRequest.class);
         ApiResponse apiResponse = new ApiResponse();
+        OrderResponse orderResponse;
 
         //判断json格式参数是否有误
         if (orderRequest == null) {
-            warpJsonParseErrorInfo(apiResponse);
-
+            setParseError(apiResponse);
             return apiResponse;
         }
 
-        OrderResponse orderResponse = orderService.queryOrderById(orderRequest.getId());
-
-        //如果处理失败 则返回错误信息
-        if (!executeIsSuccess(orderResponse.getResCode())) {
-            warpBussinessErrorInfo(apiResponse, orderResponse);
-
-            return apiResponse;
+        try {
+            orderResponse = orderService.queryOrderById(orderRequest.getId());
+            //如果处理失败 则返回错误信息
+            if (!isServiceCallSuccess(orderResponse.getResCode())) {
+                copyResponse(apiResponse, orderResponse);
+            } else {
+                apiResponse.setData(orderResponse.getOrder());
+            }
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+            setInternalError(apiResponse);
         }
-
-        apiResponse.setData(orderResponse.getOrder());
 
         return apiResponse;
     }
