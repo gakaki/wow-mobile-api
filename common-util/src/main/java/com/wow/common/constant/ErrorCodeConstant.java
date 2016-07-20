b03da58a1f7b0672f545ee85bf94cf105d638dd1
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-20 12:50:45 +0800

    常量类迁移到common

---
+++ common-util/src/main/java/com/wow/common/constant/ErrorCodeConstant.java
@@ -0,0 +1,13 @@
+package com.wow.common.constant;
+
+/**
+ * Created by zhengzhiqing on 16/7/12.
+ */
+public interface ErrorCodeConstant {
+    public static final String SUCCESS = "0";
+    public static final String INVALID_PARAMETER = "40000";
+    public static final String INVALID_PARAMJSON = "40001";
+    public static final String INTERNAL_ERROR = "50000";
+    public static final String INVALID_TOKEN = "10000";
+
+}
