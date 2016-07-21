dfdefbc16ac855721ec5736b9afa37dbd56dc9c9
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-21 14:53:23 +0800

    修正微信bug

---
+++ src/main/java/com/wow/mobileapi/request/user/CheckWcOpenIdBindedRequest.java
@@ -0,0 +1,23 @@
+package com.wow.mobileapi.request.user;
+
+import com.wow.common.request.ApiRequest;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by zhengzhiqing on 16/6/28.
+ */
+public class CheckWcOpenIdBindedRequest extends ApiRequest {
+
+    private static final long serialVersionUID = 1L;
+    @NotNull
+    private String openId;
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+}
