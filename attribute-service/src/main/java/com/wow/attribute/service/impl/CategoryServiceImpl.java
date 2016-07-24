fcae707b1b8d2383a84392d2cf4d98319e8040bd
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-24 17:17:49 +0800

    修改实现方法

--- attribute-service/src/main/java/com/wow/attribute/service/impl/CategoryServiceImpl.java
+++ attribute-service/src/main/java/com/wow/attribute/service/impl/CategoryServiceImpl.java
@@ -1,14 +1,5 @@
 package com.wow.attribute.service.impl;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.wow.attribute.mapper.CategoryMapper;
 import com.wow.attribute.model.Category;
 import com.wow.attribute.model.CategoryExample;
@@ -18,6 +9,13 @@ import com.wow.attribute.vo.response.CategoryResponse;
 import com.wow.common.response.CommonResponse;
 import com.wow.common.util.CollectionUtil;
 import com.wow.common.util.ErrorCodeUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 类目服务
@@ -29,9 +27,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
     private CategoryMapper categoryMapper;
+
     //一级分类的父分类:0
     private final int PARENT_CATEGORY_OF_FIRST_LEVEL_CATEGORY=0;
-    private StringBuffer categoryIds;
+
 
     /**
      * 创建类目
@@ -153,69 +152,40 @@ public class CategoryServiceImpl implements CategoryService {
      */
     @Override
     @Transactional(propagation= Propagation.NOT_SUPPORTED)
-    public String getLastLevelCategoryByCategory(int categoryId) {
-        //TODO:
-        //如果该分类已经是第三级(也是最后一级),返回该分类本身
+    public List<Integer> getLastLevelCategoryByCategory(int categoryId) {
+
+        List<Integer> categoryIdList = new ArrayList<>();
+
+        //TODO: category_id必须>0
+        //TODO: 如果该分类已经是第三级(也是最后一级),返回该分类本身
+
         //否则,递归查询该分类的子分类,直到最后一级
-    	categoryIds = new StringBuffer();
+        //TODO: 要从缓存(redis)读取
     	List<Category> categoryListAll = categoryMapper.selectAllCategory();
-    	String childId = getChildNodes(categoryListAll,categoryId);
-    	if(childId.length()>0)
-    		childId = childId.substring(0,childId.length()-1);
-        return childId;
+    	categoryIdList = getChildCategories(categoryListAll,categoryId);
+
+        return categoryIdList;
     }
     
     /**
      * 根据父节点的ID获取所有子节点
-     * @param list 分类表
-     * @param typeId 传入的父节点ID
+     * @param categoryList 分类表
+     * @param parentCategoryId 传入的父节点ID
      * @return String
      */
-    private String getChildNodes(List<Category> list, Integer typeId) {
-        if(list == null && typeId == null) return "";
-        for (Iterator<Category> iterator = list.iterator(); iterator.hasNext();) {
-        	Category node = (Category) iterator.next();
-            // 一、根据传入的某个父节点ID,遍历该父节点的所有子节点
-            if (typeId==node.getParentCategoryId()) {
-                recursionFn(list, node);
-            }
-            // 二、遍历所有的父节点下的所有子节点
-            /*if (node.getParentCategoryId()==0) {
-                recursionFn(list, node);
-            }*/
-        }
-        return categoryIds.toString();
-    }
-     
-    private void recursionFn(List<Category> list, Category node) {
-        List<Category> childList = getChildList(list, node);// 得到子节点列表
-        if (hasChild(list, node)) {// 判断是否有子节点
-        	categoryIds.append(node.getId()).append(",");
-            Iterator<Category> it = childList.iterator();
-            while (it.hasNext()) {
-            	Category n = (Category) it.next();
-                recursionFn(list, n);
-            }
-        } else {
-        	categoryIds.append(node.getId()).append(",");
-        }
-    }
-     
-    // 得到子节点列表
-    private List<Category> getChildList(List<Category> list, Category node) {
-        List<Category> nodeList = new ArrayList<Category>();
-        Iterator<Category> it = list.iterator();
-        while (it.hasNext()) {
-        	Category n = (Category) it.next();
-            if (n.getParentCategoryId() == node.getId()) {
-                nodeList.add(n);
+    public static List<Integer> getChildCategories(List<Category> categoryList, Integer parentCategoryId) {
+        if(CollectionUtil.isEmpty(categoryList)) return null;
+        List<Integer> subCategoryList = new ArrayList<>();
+
+        for (Category category : categoryList) {
+            if (category.getParentCategoryId()==parentCategoryId) {
+                if (category.getCategoryLevel()==3) {
+                    subCategoryList.add(category.getId());
+                } else {
+                    subCategoryList.addAll(getChildCategories(categoryList, category.getId()));
+                }
             }
         }
-        return nodeList;
-    }
- 
-    // 判断是否有子节点
-    private boolean hasChild(List<Category> list, Category node) {
-        return getChildList(list, node).size() > 0 ? true : false;
+        return subCategoryList;
     }
 }
