3d4a9a34e1591f9ce348a874a04b4501fc23da10
zhengzhiqing <zhengzhiqing@wowdsgn.com>
2016-07-27 10:13:13 +0800

    图片增加前缀

--- src/main/java/com/wow/mobileapi/controller/DesignerController.java
+++ src/main/java/com/wow/mobileapi/controller/DesignerController.java
@@ -1,21 +1,21 @@
 package com.wow.mobileapi.controller;
 
+import com.wow.common.request.ApiRequest;
+import com.wow.common.response.ApiResponse;
+import com.wow.common.util.JsonUtil;
 import com.wow.common.util.StringUtil;
 import com.wow.common.util.ValidatorUtil;
+import com.wow.mobileapi.request.product.ProductQueryRequest;
+import com.wow.mobileapi.util.ImgPrefixUtil;
+import com.wow.product.model.Designer;
+import com.wow.product.service.DesignerService;
+import com.wow.product.vo.response.ProductDesignerResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.wow.common.request.ApiRequest;
-import com.wow.common.response.ApiResponse;
-import com.wow.common.util.JsonUtil;
-import com.wow.mobileapi.request.product.ProductQueryRequest;
-import com.wow.product.model.Designer;
-import com.wow.product.service.DesignerService;
-import com.wow.product.vo.response.ProductDesignerResponse;
 
 /**
  * Created by zhengzhiqing on 16/6/23.
@@ -53,11 +53,11 @@ public class DesignerController extends BaseController {
 
         try {
         	Designer designer = designerService.getDesignerById(productQueryRequest.getDesignerId());
-            
+            //增加前缀
+            designer.setDesignerPhoto(ImgPrefixUtil.addPrefixForImgUrl(designer.getDesignerPhoto()));
         	ProductDesignerResponse productDesignerResponse = new ProductDesignerResponse();
         	productDesignerResponse.setDesigner(designer);
-            apiResponse.setData(productDesignerResponse);
-            
+            apiResponse.setData(productDesignerResponse.getDesigner());
         } catch (Exception e) {
             logger.error("查找productDesigner_detail错误---" + e);
             setInternalErrorResponse(apiResponse);
