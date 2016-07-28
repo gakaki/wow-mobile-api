100768c80338ca5e8bf5156ed46ff1804a012308
kaiwei <kaiweichen_happy@126.com>
2016-07-28 10:24:14 +0800

    删除多余的图片工具包

--- src/main/java/com/wow/mobileapi/controller/ProductController.java
+++ src/main/java/com/wow/mobileapi/controller/ProductController.java
@@ -1,16 +1,42 @@
 package com.wow.mobileapi.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.wow.attribute.model.Category;
 import com.wow.attribute.service.CategoryService;
 import com.wow.attribute.vo.response.CategoryListResponse;
 import com.wow.common.page.PageModel;
 import com.wow.common.request.ApiRequest;
 import com.wow.common.response.ApiResponse;
-import com.wow.common.util.*;
+import com.wow.common.util.BeanUtil;
+import com.wow.common.util.CollectionUtil;
+import com.wow.common.util.ErrorCodeUtil;
+import com.wow.common.util.ErrorResponseUtil;
+import com.wow.common.util.ImgPrefixUtil;
+import com.wow.common.util.JsonUtil;
+import com.wow.common.util.MapUtil;
+import com.wow.common.util.StringUtil;
+import com.wow.common.util.ValidatorUtil;
 import com.wow.mobileapi.request.product.ProductInfoRequest;
 import com.wow.mobileapi.request.product.ProductQueryRequest;
-import com.wow.mobileapi.response.product.*;
-import com.wow.mobileapi.util.ImgPrefixUtil;
+import com.wow.mobileapi.response.product.ColorMapVo;
+import com.wow.mobileapi.response.product.ColorSpecVo;
+import com.wow.mobileapi.response.product.ItemDetailResponse;
+import com.wow.mobileapi.response.product.ItemSpecResponse;
+import com.wow.mobileapi.response.product.ProductImageResponse;
+import com.wow.mobileapi.response.product.SpecColorVo;
+import com.wow.mobileapi.response.product.SpecMapVo;
+import com.wow.mobileapi.response.product.SubProductInfo;
 import com.wow.price.model.ProductPrice;
 import com.wow.price.service.PriceService;
 import com.wow.price.vo.ProductListPriceResponse;
@@ -31,17 +57,6 @@ import com.wow.product.vo.response.ProductResponse;
 import com.wow.stock.service.StockService;
 import com.wow.stock.vo.AvailableStockVo;
 import com.wow.stock.vo.response.AvailableStocksResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class ProductController extends BaseController {
@@ -232,7 +247,7 @@ public class ProductController extends BaseController {
                     availableStockMap.put(availableStockVo.getProductId(), availableStockVo.getTotalAvailableStockQty());
                 }
             }
-            Map<Integer, ProductPrice> priceMap = new HashMap();
+            Map<Integer, ProductPrice> priceMap = new HashMap<Integer, ProductPrice>();
             //批量查询价格
             ProductListPriceResponse productPriceListResponse = priceService.getProductPriceList(subProductIds);
             if (productPriceListResponse != null && MapUtil.isNotEmpty(productPriceListResponse.getMap())) {
