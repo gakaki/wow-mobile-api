b12d1ac3521f35bec6273808903f4e64273c24a9
felix <wangyanlong@wowdsgn.com>
2016-08-07 11:59:22 +0800

    修改获取分类,添加背景图

--- src/main/java/com/wow/mobileapi/controller/CategoryController.java
+++ src/main/java/com/wow/mobileapi/controller/CategoryController.java
@@ -4,6 +4,7 @@ package com.wow.mobileapi.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.wow.attribute.vo.response.CategoryResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
