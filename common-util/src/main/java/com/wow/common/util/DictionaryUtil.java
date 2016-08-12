9ec6f6110af4a28b25e86db8074b1b23df0127f5
chenkaiwei <kaiweichen_happy@126.com>
2016-08-12 16:57:56 +0800

    优化数据字典初始化大小

--- common-util/src/main/java/com/wow/common/util/DictionaryUtil.java
+++ common-util/src/main/java/com/wow/common/util/DictionaryUtil.java
@@ -26,7 +26,7 @@ public class DictionaryUtil {
     //存放组对应的数据字典信息
     private static final Map<String, List<Dictionary>> groupMap = new HashMap<String, List<Dictionary>>();
     //存放key对应的数据字典信息
-    private static final Map<String, Dictionary> keyMap = new HashMap<String, Dictionary>(300);
+    private static final Map<String, Dictionary> keyMap = new HashMap<String, Dictionary>(50);
 
     /**
      * 初始化数据字典信息
